3c0bc5048dd0289a2450af5e61df21f01eca6413
Solomon40 <tochukwuewiwilem@gmail.com>
2020-12-10 09:43:42 -0800

    Successfully set up npm and Express Server

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,19 @@
+let express = require('express'),
+    path = require('path'),
+    open = require('open');
+
+const port = 3000;
+const app = express();
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err){
+    if (err){
+        console.log(err);
+    }
+    else{
+        open(`http://localhost:` + port);
+    }
+});
