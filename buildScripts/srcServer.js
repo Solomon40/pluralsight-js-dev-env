d6262b91d67428965934986bb84a965d43644ef7
Solomon40 <tochukwuewiwilem@gmail.com>
2021-01-12 07:10:29 -0800

    configured webpack with Express

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,17 @@
 let express = require('express'),
     path = require('path'),
     open = require('open');
+import webpack from 'webpack';
+import config from '../webpack_config.dev';
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: 'true',
+    publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
@@ -11,7 +19,7 @@ app.get('/', function(req, res){
 
 app.listen(port, function(err){
     if (err){
-        console.log(err); 
+        console.log(err);
     }
     else{
         open(`http://localhost:${port}`);
